4afb8f2ce468f31cfe89964af734c7171b657d45
ErlendTLygre <erlend.tl2@gmail.com>
2020-03-04 13:47:29 +0100

    add buttons on video to frontpage. Change navbar size

---
+++ src/components/organisms/CTASection.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+
+const CTAContainer = styled.div`
+    width: 100%;
+    height: 300px;
+    background-color: rgba(255, 255, 255, 0.1);
+`
+
+const CTASection = () => {
+    
+    return (
+        <CTAContainer>
+
+        </CTAContainer>
+    )
+}
+
+export default CTASection
