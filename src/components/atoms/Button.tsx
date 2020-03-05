ba2f6d65f4025b7acdaa5bf7c61e98dc1809f3d7
ErlendTLygre <erlend.tl2@gmail.com>
2020-03-05 11:23:26 +0100

    add basic footer

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,6 +4,7 @@ const Button = styled.button`
     width: 100%;
     background-color: #fa5333;
     font-family: 'Changa One';
+    border: none;
     color: snow;
     font-size: 1.3rem;
     text-transform: uppercase;
